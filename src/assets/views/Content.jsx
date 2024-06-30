32c951068df406f027a511b8e02790402364a725
CsBenedek32 <csapobenedek32@gmail.com>
2024-06-30 18:52:10 +0200

    Init

---
+++ src/assets/views/Content.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OffcanvasComp from "../components/OffcanvasComp";
+
+const Content = () => {
+  return (
+    <div>
+      <OffcanvasComp></OffcanvasComp>
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/assets/views/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <div className="fs-6">Layout</div>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
